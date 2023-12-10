a24029bffe6b94751698fe70c16bf2923f7c1c90
MMargin89 <153352084+MMargin89@users.noreply.github.com>
2023-12-10 14:43:21 +0500

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,5 @@
+# Михаил Маргин, 11-я когорта - Дипломный проект. Инженер по тестированию плюс
+
+URL_SERVICE = "https://390a667b-53cf-4c6a-8385-848fc3a6e050.serverhub.praktikum-services.ru/"
+CREATE_ORDER_PATH = "/api/v1/orders"
+GET_ORDER_INFO = "/api/v1/orders/track"
